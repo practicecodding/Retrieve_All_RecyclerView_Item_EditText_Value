b59e4e8533d7f19b5953bf0db174d23d77099561
S.M.Hamidul <smhamidul6195@gmail.com>
2024-10-14 07:09:19 +0600

    Get All RecyclerView Item EditText Value

--- app/src/main/java/com/hamidul/recyclerviewedittextitemvalueget/activities/MainActivity.java
+++ app/src/main/java/com/hamidul/recyclerviewedittextitemvalueget/activities/MainActivity.java
@@ -85,6 +85,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     //==============================================================================================
 
     void swipe(){
